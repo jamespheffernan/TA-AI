02043587c08b8ff65248b4b68e84133b0c403306
jamespheffernan <jamespheffernan@me.com>
2025-08-04 15:54:43 +0100

    chore(security): add NSG and subnet associations for VNet module

--- infrastructure/modules/vnet/main.tf
+++ infrastructure/modules/vnet/main.tf
@@ -30,6 +30,51 @@ resource "azurerm_subnet" "functions" {
   address_prefixes     = ["10.0.3.0/24"]
 }
 
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Deny-Internet-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-All-Outbound"
+    priority                   = 200
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "postgres" {
+  subnet_id                 = azurerm_subnet.postgres.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "openai" {
+  subnet_id                 = azurerm_subnet.openai.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "functions" {
+  subnet_id                 = azurerm_subnet.functions.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
 output "postgres_subnet_id" {
   value = azurerm_subnet.postgres.id
 }
